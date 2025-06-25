c0aaa80f3779f0d107d9ba5c8628ffe1c800f4d7
Cory Adkins <adkins.cory@gmail.com>
2025-06-25 00:03:26 -0400

    set up pins upload/download

---
+++ R/commit_data.R
@@ -0,0 +1,26 @@
+pacman::p_load("here", "pins", "readr")
+board <- board_s3(
+  bucket = Sys.getenv("S3_BUCKET"),
+  versioned = TRUE,
+  cache = here("cache"),
+  region = Sys.getenv("S3_REGION"),
+  endpoint = Sys.getenv("S3_ENDPOINT"),
+  access_key = Sys.getenv("S3_PUB_KEY"),
+  secret_access_key = Sys.getenv("S3_PRIV_KEY")
+)
+
+df <- read_csv(here("data", "resume_data.csv"),
+              col_types = cols(exp_type = "c", 
+              title = "c", location = "c", date = "D", 
+              start = "D", end = "D", description = "c", 
+              detail1 = "c", detail2 = "c", detail3 = "c",
+              detail4 = "c"))
+pin_write(
+  board,
+  df,
+  name = "resume_data",
+  type = "parquet",
+  title = "Corys Resume Data",
+  description = "Raw data for generating a quarto resume that describes education, skills and work experience",
+  tags = "default"
+)
