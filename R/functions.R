ad78f94e5ddb4cba5db9322cf98471f129942f80
Cory Adkins <adkins.cory@gmail.com>
2025-06-27 23:13:14 -0400

    add functions for loading and uploading data

--- R/functions.R
+++ R/functions.R
@@ -1,31 +1,94 @@
-resume_entry_skills <- function(data,
-                         title = "title",
-                         details = "bullets_parsed") {
-  strings <- apply(data, 1, function(row) {
-    if (!is.na(row[title])) {
-      s <- sprintf("(\"%s\", (", row[title])
-    }
-    if (!is.na(row[details])) {
-      t <- sapply(row[details], \(x) {
-           sprintf("[%s],", x)
+# Functions for setting up pins and S3
+set_up_board <- function(
+  bucket = Sys.getenv("S3_BUCKET"),
+  versioned = TRUE,
+  cache = here("cache"),
+  region = Sys.getenv("S3_REGION"),
+  endpoint = Sys.getenv("S3_ENDPOINT"),
+  access_key = Sys.getenv("S3_PUB_KEY"),
+  secret_access_key = Sys.getenv("S3_PRIV_KEY")
+) {
+  board <- board_s3(
+    bucket = bucket,
+    versioned = versioned,
+    cache = cache,
+    region = region,
+    endpoint = endpoint,
+    access_key = access_key,
+    secret_access_key = secret_access_key
+  )
+  return(board)
+}
+# Functions for loading and uploading raw resume data
+## `read_resume_data()` reads resume data with defaults that match the .csv template
+read_resume_data <- function(
+  file = here("data", "resume_data.csv"),
+  col_types = cols(
+    exp_type = "c",
+    title = "c",
+    location = "c",
+    date = "D",
+    start = "D",
+    end = "D",
+    bullets = "c"
+  )
+) {
+  out <- readr::read_csv(file = file, col_types = col_types)
+  return(out)
+}
+## `resume_pin_write()` writes the pins with preferred defaults for this project
+resume_pin_write <- function(
+  board = board,
+  data,
+  name = "resume_data",
+  type = "parquet",
+  title = "Corys Resume Data",
+  description = "Raw data for generating a quarto resume that describes education, skills and work experience",
+  tags = "default"
+) {
+  pin_write(
+    board = board,
+    x = data,
+    name = name,
+    type = type,
+    title = title,
+    description = description,
+    tags = tags 
+  )
+}
+# Functions for creating resume entries
+## `parse_bullets()` transforms lists of resume item details into a usable format
+parse_bullets <- function(
+  df,
+  bullet_col = "bullets",
+  output_col = "bullets_parsed"
+) {
+  df |>
+    dplyr::mutate(
+      !!output_col := map(.data[[bullet_col]], \(x) {
+        items <- strsplit(x, "\n")
+        items <- sapply(
+          items,
+          \(x) gsub("^\\s*-\\s*", "", x),
+          USE.NAMES = FALSE
+        )
+        items <- sapply(items, trimws, USE.NAMES = FALSE)
+        items <- items[items != ""]
+        return(items)
       })
-      t <- c(t, ")),")
-      t <- paste0(t, collapse = "\n")
-    }
-     s <- paste(s, t, sep = "\n") 
-     return(s)
-  })
-  cat(paste0("```{=typst}\n", "#skills-entry((\n", 
-  paste(strings, collapse = "\n"), "\n))", "\n```"))
+    )
 }
 
-resume_entry_edu <- function(data,
-    title = "title",
-    location = "location",
-    date = "date",
-    description = "description",
-    details = "bullets_parsed"){
- strings <- apply(data, 1, function(row) {
+## 'resume_entry_default()` generates typst code for education and experience entries from a data frame
+resume_entry_default <- function(
+  data,
+  title = "title",
+  location = "location",
+  date = "date",
+  description = "description",
+  details = "bullets_parsed"
+) {
+  strings <- apply(data, 1, function(row) {
     s <- "#resume-entry("
     if (!is.na(row[title])) {
       s <- sprintf("%stitle: \"%s\",", s, row[title])
@@ -43,27 +106,42 @@ resume_entry_edu <- function(data,
 
     if (!is.na(row[details])) {
       t <- sapply(row[details], \(x) {
-           sprintf("[%s],", x)
+        sprintf("[%s],", x)
       })
-      t <- paste0(t, collapse="\n")
+      t <- paste0(t, collapse = "\n")
     }
-     t <- paste0("#resume-item((", t, "))")
-     s <- paste(s, t, sep = "\n") 
+    t <- paste0("#resume-item((", t, "))")
+    s <- paste(s, t, sep = "\n")
     return(s)
   })
   cat(paste0("```{=typst}\n", paste(strings, collapse = "\n"), "\n```"))
 }
 
-  parse_bullets <- function(df, bullet_col = "bullets", output_col = "bullets_parsed") {
-  df |>
-    dplyr::mutate(
-      !!output_col := map(.data[[bullet_col]], \(x) {
-        items <- strsplit(x, "\n")
-        items <- sapply(items, \(x) gsub("^\\s*-\\s*", "", x), 
-                       USE.NAMES = FALSE)
-        items <- sapply(items, trimws, USE.NAMES = FALSE)
-        items <- items[items != ""]
-        return(items)
+## `resume_entry_skills()` generates typst code for skill listings from a data frame of resume items
+resume_entry_skills <- function(
+  data,
+  title = "title",
+  details = "bullets_parsed"
+) {
+  strings <- apply(data, 1, function(row) {
+    if (!is.na(row[title])) {
+      s <- sprintf("(\"%s\", (", row[title])
+    }
+    if (!is.na(row[details])) {
+      t <- sapply(row[details], \(x) {
+        sprintf("[%s],", x)
       })
-    )
-  }
+      t <- c(t, ")),")
+      t <- paste0(t, collapse = "\n")
+    }
+    s <- paste(s, t, sep = "\n")
+    return(s)
+  })
+  cat(paste0(
+    "```{=typst}\n",
+    "#skills-entry((\n",
+    paste(strings, collapse = "\n"),
+    "\n))",
+    "\n```"
+  ))
+}
